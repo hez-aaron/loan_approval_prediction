eff0e4d66d54290b690c0e08139a3009f383d2b1
Hezekiah E. Aaron <hezaaron21@gmail.com>
2026-04-11 17:47:37 +0100

    Initial project version

---
+++ quote_guessing_game.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+from time import sleep
+from random import choice
+
+# create an empty list to store scraped data
+all_quote = []
+
