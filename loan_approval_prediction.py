7b134864f0827dd8a7f316f25498b9068794684e
Hezekiah E. Aaron <hezaaron21@gmail.com>
2026-04-15 18:25:51 +0100

    Convert categorical values into numerical labels

--- loan_approval_prediction.py
+++ loan_approval_prediction.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn import preprocessing
 
 data = pd.read_csv('LoanApprovalPrediction.csv')
 print(data.head())
@@ -13,16 +14,25 @@ print('Categorical variables:', len(list(str_data[str_data].index)))
 # Loan_ID is not correlated with any of the other columns
 data.drop(['Loan_ID'], axis=1, inplace=True) 
 
-# Visualize categorical values in using barplot
-str_data = (data.dtypes == 'str')
-string_cols = list(str_data[str_data].index)
+# Visualize categorical values in columns using barplot
+string_data = (data.dtypes == 'str')
+string_columns = list(string_data[string_data].index)
 fig, axs = plt.subplots(2, 3, figsize=(9, 6), dpi=95)
 
 for i , ax in enumerate(axs.flatten()):
-    y = data[string_cols[i]].value_counts()
+    cnt = data[string_columns[i]].value_counts()
     plt.xticks(rotation=90)
-    sns.barplot(x=list(y.index), y=y, ax=ax)
-    ax.set_title(string_cols[i])
+    sns.barplot(x=list(cnt.index), y=cnt, ax=ax)
+    ax.set_title(string_columns[i])
 
 plt.tight_layout()
 plt.show()
+
+# Convert categorical values into numerical labels
+label_encoder = preprocessing.LabelEncoder()
+for col in string_columns:
+    data[col] = label_encoder.fit_transform(data[col])
+
+# Check to ensure that there are no string data colums
+string_data = (data.dtypes == 'str')
+print('Categorical variables:', len(list(string_data[string_data].index)))
