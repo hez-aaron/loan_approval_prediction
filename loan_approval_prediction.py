8d25a78e370d4c594ab0a0670a853dfe6f09350b
Hezekiah E. Aaron <hezaaron21@gmail.com>
2026-04-17 17:54:35 +0100

    Create heatmap to visualize correlation

--- loan_approval_prediction.py
+++ loan_approval_prediction.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
 
+# Get and view data
 data = pd.read_csv('LoanApprovalPrediction.csv')
 print(data.head())
 
-# Get the number of columns with string data
-str_data = (data.dtypes == 'str')
-print('Categorical variables:', len(list(str_data[str_data].index)))
+def get_string_data():
+    return (data.dtypes == 'str')
 
-# Loan_ID is not correlated with any of the other columns
-data.drop(['Loan_ID'], axis=1, inplace=True) 
+def get_string_columns():
+    return list(get_string_data()[get_string_data()].index)
 
-# Visualize categorical values in columns using barplot
-string_data = (data.dtypes == 'str')
-string_columns = list(string_data[string_data].index)
-fig, axs = plt.subplots(2, 3, figsize=(9, 6), dpi=95)
+print('Categorical variables:', len(get_string_columns())) # Number of columns with categorical values
+
+data.drop(['Loan_ID'], axis=1, inplace=True) # Loan_ID is not correlated with any of the other columns,so we drop it
+
+# Visualize categorical values in columns with barplot
+string_columns = get_string_columns()
+fig, axs = plt.subplots(2, 3, figsize=(8, 5), dpi=95)
 
 for i , ax in enumerate(axs.flatten()):
     cnt = data[string_columns[i]].value_counts()
@@ -33,6 +36,10 @@ label_encoder = preprocessing.LabelEncoder()
 for col in string_columns:
     data[col] = label_encoder.fit_transform(data[col])
 
-# Check to ensure that there are no string data colums
-string_data = (data.dtypes == 'str')
-print('Categorical variables:', len(list(string_data[string_data].index)))
+print('Categorical variables:', len(get_string_columns())) # Check to ensure that there are no string data columns
+
+# Visualize correlations with heatmap
+plt.figure(figsize=(10,6))
+sns.heatmap(data.corr(), cmap='PRGn', fmt='.2f', linewidths=2, annot=True)
+plt.tight_layout()
+plt.show()
